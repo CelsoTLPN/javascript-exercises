c5d008e001668ad9fd17fc286fd9c38fdd7cf2a8
CelsoTLPN <celsotlpn@gmail.com>
2022-10-14 10:14:26 -0300

    finished calculator exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+
+const subtract = function(c, d) {
+	return c - d;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  const result = 0;
+	const sumAll = arr.reduce((a, b) => a + b, result)
+  return sumAll;
 };
 
-const multiply = function() {
 
+const multiply = function(arr) {
+  let result = 1;
+	const multAll = arr.reduce((a, b) => a * b, result)
+  return multAll;
 };
 
-const power = function() {
-	
+
+const power = function(a, b) {
+  return a ** b;
 };
 
-const factorial = function() {
-	
+
+const factorial = function(fact) {
+  let result = 1;
+	for (let i = 1; i <= fact; i++) {
+    result *= i;
+  }
+  return result;
 };
 
 // Do not edit below this line
